e71c1a8927e50460f30ebf94feb573fdc478f21b
M.Azer <messaoud.azer.1@gmail.com>
2024-07-24 15:59:03 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LevelListComponent } from './level-list/level-list.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { LevelCrudComponent } from './level-crud/level-crud.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LevelListComponent,
+    LevelCrudComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration()
+    [provideHttpClient(withFetch())]
   ],
   bootstrap: [AppComponent]
 })
